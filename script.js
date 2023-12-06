805e0e46a313dfbdaa40ff05c5b4d75d43ecacec
ATUL CHOURASIYA <146053182+atulchourasiya99@users.noreply.github.com>
2023-12-06 14:25:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function appendCharacter(char) {
+    document.getElementById('display').value += char;
+  }
+  
+  function calculate() {
+    try {
+      document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
+  
